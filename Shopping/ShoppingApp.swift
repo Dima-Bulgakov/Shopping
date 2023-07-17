aa2231436967ec9cda1332140061fb6d80f1e67e
Dima-Bulgakov <bulgakovdu@gmail.com>
2023-07-17 15:32:05 +0300

    Initial Commit

---
+++ Shopping/ShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingApp.swift
+//  Shopping
+//
+//  Created by Dima on 17.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
